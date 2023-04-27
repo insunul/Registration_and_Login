a33e61ea459056b98b31bf849d6fe20098ffc853
Jesan <131992542+insunul@users.noreply.github.com>
2023-04-27 23:26:43 +0600

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <?php
+ echo "<link rel='stylesheet' type='text/css' href='style.css'>";
+// define variables and set to empty values
+$name = $password = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name =  test_input($_POST["uname"]);
+  $password = test_input($_POST["psw"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+</head>
+<body>
+<?php
+$file = $name.'.txt';
+$myfile = fopen($file , "r") or header('Location: http://localhost/cv/index.html');
+$email = fgets($myfile)."<br>";
+$pas = fgets($myfile);
+fclose($myfile);
+$myfile = fopen($file , "r") or die("Unable to open file!");
+$email = fgets($myfile)."<br>";
+$pass = fgets($myfile)."<br>";
+$institute = fgets($myfile)."<br>";
+$degree = fgets($myfile)."<br>";
+$session = fgets($myfile)."<br>";
+fclose($myfile);
+if ($pas == $password) {
+echo "<div class='container'>";
+echo "<img class='profile-pic' src='"."/".$name.".jpg"."' alt='Profile Picture'>";
+echo "<h1>".$name."</h1>";
+echo "<center>".$email."</center>";
+echo "<h2>Web Developer</h2>";
+echo "<div class='skills'>";
+echo "<h3>Skills</h3>";
+echo "<ul>";
+echo "<li>HTML</li>";
+echo "<li>CSS</li>";
+echo "<li>JavaScript</li>";
+echo "<li>php</li>";
+echo "<li>python</li>";
+echo "<li>c++</li>";
+echo "<li>MongoDB</li>";
+echo "</ul>";
+echo "</div>";
+echo "<div class='education'>";
+echo "<h3>Education</h3>";
+echo "<div class='degree'>".$degree."</div>";
+echo "<div class='school'>".$institute."</div>";
+echo "<div class='date'>Session ".$session."</div>";
+echo "</div>";
+echo "</div>";
+} else {
+  header('Location: http://localhost/cv/index.html');
+  exit();
+}
+?>
+</body>
+</html>
---
+++ page.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+</head>
+<body>
+<?php
+// define variables and set to empty values
+$name = $email = $password = $repassword = $institute = $degree = $session = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name =  test_input($_POST["uname"]);
+  $email = test_input($_POST["email"]);
+  $password = test_input($_POST["psw"]);
+  $repassword = test_input($_POST["psw-repeat"]);
+  $institute = test_input($_POST["ins"]);
+  $degree = test_input($_POST["deg"]);
+  $session = test_input($_POST["ses"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+<?php
+$filename = $_FILES['fileToUpload']['name'];
+$location = $_FILES['fileToUpload']['tmp_name'];
+$file_ext =pathinfo($filename,PATHINFO_EXTENSION);
+$filename =pathinfo($filename,PATHINFO_FILENAME);
+$filename =$name.".".$file_ext;
+$location1 ='/';
+move_uploaded_file($location, $location1.$filename);
+?>
+
+<?php
+if ($password==$repassword) {
+  // code...
+  $file = $name . '.txt';
+  $myfile = fopen($file , "w") or die("Unable to open file!");
+  $txt = "\n";
+  fwrite($myfile, $email);
+  fwrite($myfile, $txt);
+  fwrite($myfile, $password);
+  fwrite($myfile, $txt);
+  fwrite($myfile, $institute);
+  fwrite($myfile, $txt);
+  fwrite($myfile, $degree);
+  fwrite($myfile, $txt);
+  fwrite($myfile, $session);
+  fclose($myfile);
+  header('Location: http://localhost/cv/index.html');
+  exit();
+} else {
+  header('Location: http://localhost/cv/account.html');
+  exit();
+}
+?>
+</body>
+</html>
